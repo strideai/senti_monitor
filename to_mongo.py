400a6ea1b62cde520c603dca9aa9684c91c1e738
Codeass <hjkpidiot@gmail.com>
2017-02-13 11:32:32 +0530

    Update to_mongo.py

--- to_mongo.py
+++ to_mongo.py
@@ -1,29 +1,107 @@
 import pymongo,json
+from collections import Counter 
+import subprocess as sp
 
 conn = pymongo.MongoClient('localhost',27017)              # Mongo Stuff
-conn.admin.authenticate('username', 'password')
-db = conn.news
+conn.admin.authenticate('root', 'password')
+db = conn.news2
 collection = db.business
 
-files = ['200','300','400','500','600','700','800','900','1000','1100','1200','1300','1400']
+files1 = ['biz_Data_200_2.json','biz_Data_200_3.json','biz_Data_200.json','biz_Data_200_4.json']
+files2 = ['biz_Data_400_1.json', 'biz_Data_400_2.json','biz_Data_400_3.json']
+
+files = files2 + files1
+
+total_counter = Counter()
+total_org = []
 
 for file_no in files:
-	with open('biz_Data'+file_no +'.json','r') as f:
+	print file_no
+
+	if file_no in files1:
+		r = 200
+	else:
+		r = 400
+		
+	with open(file_no,'r') as f:
 		a  = json.load(f)
+
 	title = a['Title']
 	timestamp = a['Timestamp']
-	entities = a['Entities']
+	#entities = a['Entities']
 	doc_senti = a['Doc_sentiment']
-	sent_senti = a['Sent_sentiment']
 	subject_senti = a['Subject_sentiment']
 	article = a['Article']
 
-	for i in range(100):
-		data = {'Title':title[str(i)],'Timestamp':timestamp[str(i)],'Entities':entities[str(i)],'Doc_sentiment':doc_senti[str(i)],'Sent_sentiment':sent_senti,'Subject_sentiment':subject_senti[str(i)],'Article':article[str(i)] }	
-		print i		
-		collection.insert_one(data)		
-				
+
+	for i in range(r):
+		print i
+		entities = a['Entities'][str(i)]['entities']
+		org= [j['text'] for j in entities if j['type'] == 'ORG']
 
 
+		sub = a['Subject_sentiment'][str(i)]['sentiment_map']
+		sub = filter(lambda x:x['text'] in org,sub)
+		
+		org = sorted(org,key = len)[::-1]
+		for e1 in range(len(org)-1):	
+			for e2 in range(e1+1,len(org)):
+				if org[e2] in org[e1]:
+					org[e2] = org[e1]
 
+		if '404' in article[str(i)]:
+			continue
+
+		"""Taking those orgs which have subj sentiment"""
+		k = []
+		for m in sub:
+			for n in org:
+				if m['text'] in n:
+					k.append(n)
+		org = k[::]
+		org = map(lambda x:x.lower(),org) #Convert all to lower case
+		total_counter.update(org)
+		local_count = Counter(org)
+
+		total_org += org
+		#a['entity_count'] = local_count.most_common()
+		"""calculating pos neg references for each entity in each article"""
+		posneg = []
+		for m in sub:
+			pos_count = 0
+			neg_count = 0
+			score = 0
+			if m['category'] =='positive':
+				pos_count+=1
+			elif m['category'] == 'negative':
+				neg_count+=1
+			else:
+				pos_count+=1
+				neg_count+=1
+			score = (m['positive_score'] + m['negative_score'])/2
+			temp = {'positive_count':pos_count,'negative_count':neg_count,'score':score}
+			posneg.append({m['text'].lower().strip('.'):temp})
+
+		if list(set(org)) == []:
+			continue
+
+		for m in range(len(sub)):
+			st = sub[m]['text'].lower()
+			sub[m]['text'] = st 
+		
+
+		data = {'title':title[str(i)],
+				'Timestamp':timestamp[str(i)],
+				'entities':list(set(org)),
+				'Doc_sentiment':doc_senti[str(i)],
+				'entity_sentiment':sub,
+				'entity_count':dict(local_count.most_common()),
+				'article':article[str(i)],
+				'posneg':posneg,
+				}
+		
+		collection.insert(data,check_keys=False)
+		
+#with open('all_400','w') as g:
+#	g.write(str(total_counter))
 
