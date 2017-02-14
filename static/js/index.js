538acbf91671fa89b0fd9e8414a2216da5fe309e
Vishnu Y S <ysv8152@gmail.com>
2017-02-14 19:21:41 +0530

    Angular UI

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,59 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
-import App from './static/modules/App'
+var app = angular.module('Sentiment', []);
 
-render(<App />, document.getElementById('app'))
+app.controller('sentiment_manager', ['$scope', '$http', function($scope, $http){
+	
+	$scope.expectedArticleList = [], $scope.order = 'null', $scope.topEntityList = [], $scope.result = true;
+
+	$scope.textChange = () => {
+		if ($('#articleCount').val() != "") {
+			$('#articleCount').addClass('filled');
+		} else {
+			$('#articleCount').removeClass('filled');
+		}
+	}
+
+	$scope.viewArticle = (i) => {
+		var requiredArticle = $scope.expectedArticleList[i];
+		angular.element('#articleTitle').text(requiredArticle.title);
+		angular.element('#articleBody').text(requiredArticle.article);
+		angular.element('#articleViewer').modal('show');
+	}
+
+	var topEntityFinder = (articles) => {
+		var topEntityList = {};
+		for (var i = 0; i < articles.length; i++) {
+			for (var j = 0; j < articles[i].entities.length; j++) {
+				if (topEntityList[articles[i].entities[j].text] == undefined) {
+					topEntityList[articles[i].entities[j].text] = 1;
+				} 
+				else{
+					topEntityList[articles[i].entities[j].text]++;
+				};
+			};
+		};
+		var temp = [];
+		for(var key in topEntityList){
+			temp.push({name: key, count: topEntityList[key]});
+		}
+		topEntityList = temp;
+		return topEntityList;
+	}
+
+	angular.element('#articleCountCollector').on('submit', (e) => {
+		e.preventDefault();
+		$http({
+			method: 'GET',
+			url: '/feed?offset=0&limit='+ ($scope.articleCount-1),
+		})
+		.then((resp)=> {
+			$scope.result = false;
+			$scope.expectedArticleList = resp.data;
+			$scope.topEntityList = topEntityFinder($scope.expectedArticleList);
+			console.log($scope.topEntityList);
+		}, 
+		(e)=> {
+			console.error(e)
+		})
+	});
+
+}])
